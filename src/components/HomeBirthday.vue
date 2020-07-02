e02c94d5b9f15f6756d0d4f286e2055cb827b869
Andrew Leykin <katashi1328@mail.ru>
2020-07-02 09:48:49 +0300

    change init google api

--- src/components/HomeBirthday.vue
+++ src/components/HomeBirthday.vue
@@ -8,20 +8,45 @@ moment.locale("ru");
 
 export default {
   name: "HomeBirthday",
+  data: () => ({
+    profile: null
+  }),
   methods: {
+    initClient() {
+      window.gapi.client
+        .init({
+          apiKey: "AIzaSyBo5pk_VOJ1kRVRD-cXky9ZocOHfhcx18c",
+          clientId:
+            "604341426682-9nfsbb6ovjrhf41mh6vl7ojujhomsarc.apps.googleusercontent.com",
+          discoveryDocs: [
+            "https://sheets.googleapis.com/$discovery/rest?version=v4"
+          ],
+          scope: "https://www.googleapis.com/auth/spreadsheets"
+        })
+        .then(
+          () => {
+            console.log(window.gapi.auth2.getAuthInstance().isSignedIn.get());
+            if (window.gapi.auth2.getAuthInstance().isSignedIn.get())
+              this.getList();
+          },
+          error => {
+            console.error("error", JSON.stringify(error, null, 2));
+          }
+        );
+    },
     getList() {
-      this.$getGapiClient().finally(() => {
-        window.gapi.client.sheets.spreadsheets.values
-          .get({
-            spreadsheetId: "18ligZCrsZHJtECMhWadUYbIjnRKMA_MMarqWy8_5sN0",
-            range: "Class Data!A2:B"
-          })
-          .then(res => console.log(res));
-      });
+      window.gapi.client.sheets.spreadsheets.values
+        .get({
+          spreadsheetId: "18ligZCrsZHJtECMhWadUYbIjnRKMA_MMarqWy8_5sN0",
+          range: "Class Data!A2:B"
+        })
+        .then(res => console.log(res));
     }
   },
   mounted() {
-    this.getList();
+    window.addEventListener("google-loaded", () => {
+      window.gapi.load("client:auth2", this.initClient);
+    });
     // console.log(moment("29 января", "DD MMMM"));
   }
 };
