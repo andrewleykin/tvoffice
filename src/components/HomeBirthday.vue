58a4b1b15a2b17e64956190c08d43db5bbda38cd
Andrew Leykin <katashi1328@mail.ru>
2020-07-02 08:49:44 +0300

    add finally get

--- src/components/HomeBirthday.vue
+++ src/components/HomeBirthday.vue
@@ -1,26 +1,28 @@
 <template lang="pug">
   .home-birthday birthday
-
 </template>
 
 <script>
 import moment from "moment";
 moment.locale("ru");
+
 export default {
   name: "HomeBirthday",
   methods: {
-    login() {
-      this.$getGapiClient().then(gapi => {
-        console.log("login -> gapi", gapi);
-        // gapi.sheets.spreadsheet.get(...)
-        // ...
+    getList() {
+      this.$getGapiClient().finally(() => {
+        window.gapi.client.sheets.spreadsheets.values
+          .get({
+            spreadsheetId: "18ligZCrsZHJtECMhWadUYbIjnRKMA_MMarqWy8_5sN0",
+            range: "Class Data!A2:B"
+          })
+          .then(res => console.log(res));
       });
     }
   },
   mounted() {
+    this.getList();
     // console.log(moment("29 января", "DD MMMM"));
-    // sheet id - 18ligZCrsZHJtECMhWadUYbIjnRKMA_MMarqWy8_5sN0
-    this.login();
   }
 };
 </script>
